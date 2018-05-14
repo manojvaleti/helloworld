f108e968616c054691fbf1ee1ce6e74c1d494eea
Manoj Valeti <manojvaleti7@gmail.com>
2018-05-14 13:40:37 +0530

    Initial Commit

---
+++ add.py
@@ -0,0 +1,11 @@
+#this program is to add two numbers
+#this is a function which returns the sum of two given numbers
+def addition(a,b):
+    return a+b
+
+#sample input
+a = 2
+b = 3
+
+#prints the output
+print a+b
---
+++ helloworld.py
@@ -0,0 +1,5 @@
+#i am writing a program to print hello world in python2
+#this program prints hello world
+
+
+print 'Hello World'
